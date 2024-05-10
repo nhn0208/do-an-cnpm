9f200e55e02992afdd19a60310f60771289d2390
nhn28 <nguyenhoainam020803@gmail.com>
2024-05-10 15:42:06 +0700

    4

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from './ui/button'
-import { login } from '@/app/(auth)/route'
+import { login } from '@/app/api/login/route'
 
 const LoginButton = ({username,password,handleClick}:{username : string, password: string,handleClick: any}) => {
 
